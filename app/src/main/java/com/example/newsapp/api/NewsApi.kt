04dd36eeda15508b47f62e60e85bc7243ae6ee5f
Adam <adam0galon@gmail.com>
2021-10-02 19:19:19 +0200

    DAO

---
+++ app/src/main/java/com/example/newsapp/api/NewsApi.kt
@@ -0,0 +1,32 @@
+package com.example.newsapp.api
+
+import NewsResponse
+import com.example.newsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headLines")
+    suspend fun getBrakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apikey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apikey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+}
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "85e5ca843c1847629657e406507102a0"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
